514d68bab2e43526655668bcc167f88e6e6f1d2f
Sancaralp Elmas <73790877+sancaralp@users.noreply.github.com>
2023-11-21 20:38:01 +0300

    Create and add hand animations

---
+++ VRSP/Assets/_VRSP/XRCustomization/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    [SerializeField] private InputActionProperty pinchAnimationAction;
+    [SerializeField] private InputActionProperty grabAnimationAction;
+    [SerializeField] private Animator animator;
+
+    private void OnEnable()
+    {
+        pinchAnimationAction.action.Enable();
+        grabAnimationAction.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pinchAnimationAction.action.Disable();
+        grabAnimationAction.action.Disable();
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Trigger", pinchAnimationAction.action.ReadValue<float>());
+        animator.SetFloat("Grip", grabAnimationAction.action.ReadValue<float>());
+    }
+}
