8c93279987095e2f1dc8d65daff85dad7c9701cf
Sancaralp Elmas <73790877+sancaralp@users.noreply.github.com>
2024-01-12 04:27:44 +0300

    first scene finished

---
+++ VRSP/Assets/_VRSP/Scripts/Utils/SceneManagerUtil.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManagerUtil : MonoBehaviour
+{
+    public void ChangeScene(int targetScene)
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void ChangeScene(string targetScene)
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void MoveToNextScene()
+    {
+        ChangeScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+}
